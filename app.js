954acca37ff9958bd70876a37e90993a4a350495
MOHD Saboor Hussain <saboorhussain129@gmail.com>
2026-06-10 19:59:08 +0530

    feat: build contact page UI layout

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-/**
- * Main application entry point
- * Express server + EJS setup
- */
-
+app.use(express.static("public"));
 const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-/**
- * Set EJS as view engine
- */
 app.set("view engine", "ejs");
 
-/**
- * Home route
- * Renders UI from views/index.ejs
- */
+// allow CSS, images, JS
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-/**
- * Start server
- */
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
